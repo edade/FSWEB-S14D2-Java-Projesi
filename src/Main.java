fc95cdb3691be2ce27f3ebcb91e016b80b13c5aa
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-01-24 02:26:06 +0300

    Project is completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import enums.LampType;
+import enums.PaintColor;
+import model.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bedroom bedroom = new Bedroom("Master Bedroom", new Wall("North"), new Wall("South"),
+                new Wall("East"), new Wall("West"),
+                new Ceiling(300, PaintColor.BLUE),
+                new Bed("Traditional", 4, 75, 3, 2),
+                new Lamp(LampType.NEON, false, 4),
+                new Wardrobe(150, 220, 80),
+                new Carpet(250, 350, PaintColor.RED));
+
+        // Test et
+        System.out.println("Bedroom Name: " + bedroom.getName());
+        System.out.println("Ceiling Height: " + bedroom.getCeiling().getHeight());
+        System.out.println("Lamp Battery: " + bedroom.getLamp().isBattery());
+        System.out.println("Wardrobe Width: " + bedroom.getWardrobe().getWidth());
+        System.out.println("Carpet Color: " + bedroom.getCarpet().getColor());
     }
 }
---
+++ src/model/Wall.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void create() {
+        System.out.println("Wall has been created on: " + direction);
+    }
+
+    @Override
+    public String toString() {
+        return "Wall{" +
+                "direction='" + direction + '\'' +
+                '}';
+    }
+}
